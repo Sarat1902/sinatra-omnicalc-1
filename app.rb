fdf41d8855394c7d893ea9051c762ca8de72f15c
Sarat1902 <161099567+Sarat1902@users.noreply.github.com>
2024-04-11 21:09:39 +0000

    commit

--- app.rb
+++ app.rb
@@ -2,14 +2,6 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
-end
-
-
-get("/square/new") do
   erb(:new_square)
 end
 
