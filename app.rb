35e46cd212a9ccfb8c02d52d7b26527c8736285d
Sarat1902 <161099567+Sarat1902@users.noreply.github.com>
2024-04-11 21:14:09 +0000

    commit

--- app.rb
+++ app.rb
@@ -10,3 +10,13 @@ get("/square/results") do
   @results = @num ** 2
   erb(:square_results)
 end
+
+get("/square_root/new") do
+  erb(:new_square_root)
+end
+
+get("/square_root/results") do
+  @num = params.fetch("num").to_f
+  @results = @num ** 0.5
+  erb(:square_root_results)
+end
