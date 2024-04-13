370dbcdd5ec7cf3713b49d1a34f57ea151fff69b
Sarat1902 <161099567+Sarat1902@users.noreply.github.com>
2024-04-13 00:33:52 +0000

    commit

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require "sinatra"
 require "sinatra/reloader"
+require "active_support/all"
+require "./app"
 
 get("/") do
   erb(:new_square)
@@ -25,3 +27,28 @@ get("/payment/new") do
   erb(:new_payment)
 end
 
+get("/payment/results") do
+  @interest = params.fetch("interest").to_f 
+  @rate = @interest.to_fs(:percentage,{:precision => 4})
+
+  @r = @interest / 12 / 100
+
+  @years = params.fetch("years").to_i
+  @n = @years * 12
+
+  @principal = params.fetch("principal").to_f
+  @p = @principal.to_fs(:currency)
+  
+  @numerator = @principal * @r
+  
+  @d =  1 + @r
+  @db = @d ** @n
+  @dr = 1 / @db
+  @denom = 1 - @dr
+  
+  @payment = @numerator/@denom
+  @results = @payment.to_fs(:currency, {:precision => 2})
+
+  erb(:payment_results)
+
+end
